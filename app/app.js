133797afc6913f892f0b8a396fb417ea7f06cb5e
Marc Levy <marc.levy.to@me.com>
2015-07-01 11:51:15 -0400

    Moved image handing into its own directive

--- app/app.js
+++ app/app.js
@@ -1,15 +1,35 @@
-var app = angular.module('myApp', []);
+//= require_self
+//= require_tree ./services
+//= require_tree ./directives
+//= require_tree ./resources
 
+// Define resources
+angular.module('myApp.resources', []);
+
+// Define services that use the resources
+angular.module('myApp.services', ['myApp.resources']);
+
+// Define directives that use the resources and services
+angular.module('myApp.directives', ['myApp.services', 'myApp.resources']);
+
+// Define the angular application that uses the resources, services, and directives
+var app = angular.module('myApp', ['myApp.resources', 'myApp.services', 'myApp.directives']);
+
+// Define the controller
 app.controller('myController', ['$scope', '$http', function($scope, $http) {
 
+  // Scope Variables
   $scope.currentPage = 0;
   $scope.pageSize = 20;
   $scope.movies = [];
   $scope.sortBy = 'Title';
-  $scope.numPages = function(){
+
+  // Scope Functions
+  $scope.numberOfPages = function(){
     return Math.ceil($scope.movies.length/$scope.pageSize);
   }
 
+  // Load data from feed json file
   $http.get('assets/feed.json')
        .then(function(res){
 
@@ -34,12 +54,6 @@ app.controller('myController', ['$scope', '$http', function($scope, $http) {
           // Add movies to scope
           $scope.movies = res.data.Data;
         });
-
-  var audio = document.createElement('audio');
-  $scope.audio = audio;
-  audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  audio.play();
-
 }]);
 
 app.filter('startFrom', function() {
@@ -48,3 +62,20 @@ app.filter('startFrom', function() {
       return input.slice(start);
   }
 });
+
+// Define a image viewer directive
+var imageViewerCtrl = function($scope) {};
+imageViewerCtrl.$inject = ['$scope'];
+
+var imageViewer = function() {
+  var directive = {};
+
+  directive.restrict = 'E';
+  directive.scope = { movieImage: '=' };
+  directive.templateUrl = '/app/templates/image_viewer.html';
+  directive.controller = imageViewerCtrl;
+
+  return directive;
+};
+
+app.directive('imageviewer', imageViewer);
